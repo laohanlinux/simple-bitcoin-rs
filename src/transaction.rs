7fc8c33829a73432aa386e12b1dbf301ca6f7737
Rg <daimaldd@gmail.com>
2017-10-04 21:23:21 +0800

    ypdate

--- src/transaction.rs
+++ src/transaction.rs
@@ -3,11 +3,18 @@ extern crate rust_base58;
 extern crate serde;
 extern crate serde_json;
 extern crate sha2;
+extern crate hex;
+extern crate elliptic;
+extern crate bigint;
 
+use self::sha2::{Sha256, Digest};
 use self::compare::Compare;
 use self::rust_base58::{ToBase58, FromBase58};
+use self::elliptic::curve25519;
+use self::bigint::U256;
+
 use std::cmp::Ordering::{Less, Greater};
-use self::sha2::{Sha256, Digest};
+use std::collections::HashMap;
 
 const SUBSIDY: i32 = 10;
 
@@ -21,6 +28,10 @@ struct Transaction {
 impl Transaction {
     pub fn serialize(&self) -> Vec<u8> {
         serde_json::to_string(self).unwrap().into_bytes()
+    }
+    // IsCoinbase checks whether the transaction is coinbase
+    pub fn is_coinbase(&self) -> bool {
+        self.vin.len() == 1 && self.vin[0].txid.len() == 0 && self.vin[0].vout == -1
     }
 
     // Hash returns the hash of the Transaction
@@ -56,7 +67,58 @@ impl Transaction {
     }
 
     // TrimmedCopy creates a trimmed copy of Transaction to be used in signing
-    pub fn trimmed_copy(&self) -> Self {}
+    // not include signature and pub_key.
+    pub fn trimmed_copy(&self) -> Self {
+        let mut inputs: Vec<TXInput> = vec![];
+        let mut outputs: Vec<TXOutput> = vec![];
+
+        for vin in &self.vin {
+            let tx = TXInput{
+                txid: vin.txid.clone(),
+                vout: vin.vout.clone(),
+                signature: vec![],
+                pub_key: vec![],
+            };
+            inputs.push(tx);
+        }
+
+        for vout in &self.vout {
+            outputs.push(vout.clone());
+        }
+        Transaction{id: self.id.clone(), vin: inputs, vout: outputs}
+    }
+
+    // Verify verifies signatures of Transaction inputs
+    pub fn verify(&self, prev_txs: &HashMap<String, Transaction>) -> bool {
+        if self.is_coinbase() {
+            return true;
+        }
+
+        // check input of prev output's reference
+        for vin in &self.vin {
+            if prev_txs[&hex::encode(&self.id)].id.len() == 0 {
+                panic!("ERROR: Previous transaction is not correct");
+            }
+        }
+
+        let mut tx_copy = self.trimmed_copy();
+        let mut inid_idx = 0;
+        for tx_input in tx_copy.vin.iter_mut() {
+            let prev_tx: &Transaction = prev_txs.get(&hex::encode(&tx_input.txid)).unwrap();
+            {
+//                tx_copy.vin[inid_idx].signature = vec![];
+                tx_input.vin[inid_idx].signature = vec![];
+
+                tx_copy.vin[inid_idx].pub_key = prev_tx.vout[inid_idx].pub_key_hash;
+                tx_copy.id = tx_copy.hash();
+                tx_copy.vin[inid_idx].pub_key = vec![];
+            }
+
+//            let (r:U256 , s:U256) = (0, 0);
+        }
+
+        false
+    }
 }
 
 //////////////////////////////////////////
@@ -67,7 +129,7 @@ pub struct TXInput {
     // transaction id of prev output reference
     txid: Vec<u8>,
     // index of prev output reference
-    vout: i32,
+    vout: isize,
     // signature
     signature: Vec<u8>,
     // public key
@@ -83,12 +145,12 @@ impl TXInput {
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct TXOutput {
-    value: i32,
+    value: isize,
     pub_key_hash: Vec<u8>,
 }
 
 impl TXOutput {
-    pub fn new(value: i32, address: String) -> Self {
+    pub fn new(value: isize, address: String) -> Self {
         let mut txo = TXOutput {
             value: value,
             pub_key_hash: vec![],
