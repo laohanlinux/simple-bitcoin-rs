21a2d82a166c499d6ad50fd4e7cc98ab90f7663e
Rng <daimaldd@gmail.com>
2017-10-29 00:01:35 +0800

    add hyper client

--- src/http_server.rs
+++ src/http_server.rs
@@ -2,15 +2,35 @@ extern crate shio;
 extern crate lazy_static;
 extern crate serde;
 extern crate serde_json;
+extern crate futures;
+extern crate hyper;
+extern crate tokio_core;
+extern crate slog;
+extern crate slog_term;
 
 use self::shio::prelude::*;
+use self::serde_json::Error;
+use self::hyper::Client;
+use self::hyper::{Method, Request};
+use self::hyper::header::{ContentLength, ContentType};
+use self::futures::{Future, Stream};
+use self::tokio_core::reactor::Core;
 
 use std::sync::Mutex;
 use std::io;
+use std::io::Write;
 use std::io::prelude::*;
+use std::collections::HashMap;
+use std::borrow::Cow;
+
+use transaction::Transaction;
+use log::*;
 
 lazy_static!{
     static ref KNOWN_NODES: Mutex<Vec<String>> = Mutex::new(vec!["localhost:3000".to_owned()]);
+    static ref MINING_ADDRESS: &'static str = "";
+    static ref BLOCKS_IN_TRANSIT: Vec<Vec<u8>> = vec![];
+    static ref MEMPOOL: HashMap<String, Transaction> = HashMap::new();
 }
 
 #[derive(Serialize, Deserialize, Debug, Default, Clone)]
@@ -61,7 +81,12 @@ fn handle_addr(ctx: Context) -> Response {
     if ctx.body().read_to_end(&mut body).is_err() {
         return Response::with("shift");
     }
-    Response::with("Hello")
+    let res: Result<addr, Error> = serde_json::from_slice(&body);
+    if res.is_err() {
+        return Response::with(format!("{}", res.unwrap_err()));
+    }
+
+    Response::with("")
 }
 
 pub fn node_is_known(addr: String) -> bool {
@@ -72,4 +97,38 @@ pub fn node_is_known(addr: String) -> bool {
         }
     }
     false
+}
+
+fn request_blocks() {
+    let nodes = KNOWN_NODES.lock().unwrap();
+    for node in nodes.iter() {}
+}
+
+fn send_get_block(address: String) {
+    let request = get_block { add_from: address.clone() };
+    let data = serde_json::to_vec(&request).unwrap();
+    let res = send_data(format!("{}/get_block", address.clone()), data);
+    if res.is_err() {
+        error!(LOG, "http request error {:?}", res.err());
+    } else {
+        debug!(
+            LOG,
+            "{} request success, return value {:?}",
+            address,
+            String::from_utf8_lossy(&res.unwrap())
+        );
+    }
+}
+
+fn send_data(address: String, data: Vec<u8>) -> Result<Vec<u8>, hyper::Error> {
+    let uri = format!("http://{}", address).parse()?;
+    let mut core = Core::new().unwrap();
+    let client = Client::new(&core.handle());
+    let mut req = Request::new(Method::Post, uri);
+    req.headers_mut().set(ContentType::json());
+    req.headers_mut().set(ContentLength(data.len() as u64));
+    req.set_body(data);
+    let post = client.request(req).and_then(|res| res.body().concat2());
+    let data = core.run(post)?;
+    Ok(data.to_vec())
 }
